9ad320aec7c71820b1a00f7c4b3c0ebdf954f46d
jess-klost <akloste1@gmail.com>
2026-09-20 15:06:26 -0500

    Added sink and water trough to MolarMassExperiment class

--- src/molar_mass/MolarMassExperiment.js
+++ src/molar_mass/MolarMassExperiment.js
@@ -1,6 +1,8 @@
 class MolarMassExperiment {
   scale = new Scale();
   lighter = new Lighter();
+  sink = new Sink();
+  trough = new WaterTrough();
 }
 
 var molarMassExperiment = new MolarMassExperiment();
