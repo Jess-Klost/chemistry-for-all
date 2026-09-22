a13035d2a19b12c2f5e8f017592e90815a94e784
jess-klost <akloste1@gmail.com>
2026-09-21 20:16:33 -0500

    Added thermometer class for getting ambient temperature

--- src/molar_mass/MolarMassExperiment.js
+++ src/molar_mass/MolarMassExperiment.js
@@ -2,12 +2,14 @@ import Scale from "./Scale.js";
 import Lighter from "./Lighter.js";
 import Sink from "./Sink.js";
 import WaterTrough from "./WaterTrough.js";
+import Thermometer from "./Thermometer.js";
 
 export class MolarMassExperiment {
   scale = new Scale();
   lighter = new Lighter();
   sink = new Sink();
   trough = new WaterTrough();
+  thermometer = new Thermometer();
 }
 
 export var molarMassExperiment = new MolarMassExperiment();
---
+++ src/molar_mass/Thermometer.js
@@ -0,0 +1,4 @@
+export default class Thermometer {
+  // temperature should be the ambient temperature in celsius by default
+  temperature = 23.3;
+}
