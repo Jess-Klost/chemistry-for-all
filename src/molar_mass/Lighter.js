10fe60eec89acab5a478c6d03890817be6f08314
jess-klost <akloste1@gmail.com>
2026-09-22 20:04:15 -0500

    Added release butane function to lighter

--- src/molar_mass/Lighter.js
+++ src/molar_mass/Lighter.js
@@ -9,8 +9,40 @@ export default class Lighter extends WeightedObject {
   // TODO: Find actual amount of butane in lighter
   mlOfButane = 100;
 
+  connectedTubing = null;
+
   getMass() {
     // Mass is the mass of the lighter plus the mass of the butane inside
     return this.mass + (this.mlOfButane * BUTANE_GRAMS_PER_ML);
+  }
+
+  /**
+   * Reduces mlOfButane by a value within 0.1 mL of targetIncrement.
+   * 
+   * The actual amount of butane removed is a randomly selected number between 
+   * targetIncrement - 0.1 mL and targetIncrement + 1 mL.
+   * 
+   * If connectedTubing is not null (meaning there is tubing connected to the lighter),
+   * the butane will be transferred through the tubing. Otherwise, the butane will 
+   * simply be removed from the lighter like normal.
+   *  
+   * @param {Number} targetIncrement The desired amount of butane in mL to be released from the lighter.   
+   */
+  releaseButane(targetIncrement = 0.5) {
+    // Get random number between -0.1 and 0.1, and add to targetIncrement to add
+    // some amount of error.
+    var mlToRelease = targetIncrement + (Math.random() * (0.1 - -0.1) + -0.1);
+    
+    // If trying to release more butane than is currently in the lighter, 
+    // empty exactly the amount in the lighter
+    if (mlToRelease > this.mlOfButane) {
+      mlToRelease = this.mlOfButane;
+    }
+    
+    this.mlOfButane -= mlToRelease
+    
+    if (this.connectedTubing != null) {
+      // TODO: implement transferring butane to tubing
+    }
   }
 }
