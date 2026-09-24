4d9511e84d98a179725430e6cfa4ce44b632d867
jess-klost <akloste1@gmail.com>
2026-09-23 21:16:53 -0500

    Added docs to Cylinder

--- src/molar_mass/Cylinder.js
+++ src/molar_mass/Cylinder.js
@@ -1,4 +1,5 @@
 import FillableObject from "./FillableObject.js";
+import WaterTrough from "./WaterTrough.js";
 
 export default class Cylinder extends FillableObject {
   capacity = 50;
@@ -9,32 +10,81 @@ export default class Cylinder extends FillableObject {
   // TODO: verify if this is chemically accurate
   static AMOUNT_TO_ADD_WHEN_NOT_LEVELED = Math.random();
 
+  /**
+   * The current WaterTrough that the cylinder is in.
+   * 
+   * @access private
+   * 
+   * @type {WaterTrough}
+   */
   waterTrough = null;
+
+  /**
+   * Whether or not the cylinder is currently inverted in waterTrough (i.e. in position to receive butane). 
+   * 
+   * @access private
+   * 
+   * @type {Boolean}
+   */
   isInvertedInTrough = false;
+
+  /**
+   * Whether or not the cylinder's water level is leveled with waterTrough's water level.
+   * 
+   * @access private
+   * 
+   * @type {Boolean}
+   */
   isLeveledWithTroughWater = false;
 
+  /**
+   * 
+   * @param {WaterTrough} waterTrough 
+   */
   submergeInWaterTrough(waterTrough) {
     this.waterTrough = waterTrough;
     this.isInvertedInTrough = false;
     this.isLeveledWithTroughWater = false;
   }
 
+  /**
+   * Removes the cylinder from the current WaterTrough.
+   * 
+   * If the cylinder is currently inverted, any liquid in the cylinder gets
+   * transferred to the WaterTrough.
+   */
   removeFromWaterTrough() {
     if (this.waterTrough != null && this.isInvertedInTrough) {
       this.waterTrough.addLiquid(this.removeLiquid(this.capacity));
-      this.waterTrough = null;
     }
+    this.waterTrough = null;
     this.isInvertedInTrough = false;
     this.isLeveledWithTroughWater = false;
   }
 
+  /**
+   * Inverts the cylinder in the current water trough.
+   * 
+   * Must first be submerged in a water trough. If it is not inverted already,
+   * it will transfer water from the water trough into the cylinder equal to the
+   * cylinder's capacity. 
+   */
   invertInWaterTrough() {
-    if (this.waterTrough != null) {
+    if (this.waterTrough != null && !this.isInvertedInTrough) {
       this.isInvertedInTrough = true;
       this.addLiquid(this.waterTrough.removeLiquid(this.capacity));
     }
   }
 
+  /**
+   * Sets whether or not the cylinder's water level is levelled with the water trough's water level.
+   *
+   * If the cylinder is leveled with the trough's water, getDisplacedLevel will return
+   * a more accurate reading. The cylinder must first be inverted in a WaterTrough
+   * before setting it to be leveled.
+   * 
+   * @param {Boolean} state True if cylinder should be leveled with water, false if it should be in position for transferring butane.
+   */
   setIsLeveledWithTroughWater(state) {
     // Must be inverted in trough to level it with trough water
     if (state && this.isInvertedInTrough) {
@@ -45,6 +95,18 @@ export default class Cylinder extends FillableObject {
     }
   }
 
+  /**
+   * Displaces water equal to the value of amountToTransfer.
+   * 
+   * The cylinder must be inverted in a water trough for water to be displaced
+   * when this is called. The displaced water will be removed from the cylinder
+   * and added to the water trough.
+   * 
+   * This function is intended to be called by the connected tubing and 
+   * not directly called by the lighter or view.
+   * 
+   * @param {Number} amountToTransfer The amount of water to displace due to the addition of butane.
+   */
   transferButane(amountToTransfer) {
     // Only displace water if in the proper set up
     if (this.waterTrough != null && this.isInvertedInTrough 
@@ -53,6 +115,14 @@ export default class Cylinder extends FillableObject {
     }
   }
 
+  /**
+   * Returns the current amount amount of water that has been displaced by butane.
+   *
+   * If the cylinder is not leveled with the water trough, the value returned
+   * is increased by AMOUNT_TO_ADD_WHEN_NOT_LEVELED.
+   *  
+   * @returns The current amount of water that has been displaced by butane.
+   */
   getDisplacedLevel() {
     if (this.isLeveledWithTroughWater) {
       return this.capacity - this.currentLevel;
